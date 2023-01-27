0d70ef457c76093d5905ce48e9cadad41c87a8d9
SamuelBelo7 <samuel.belo@seteloc.com.br>
2023-01-27 15:21:56 -0300

    Error handling on signIn and signUp

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,65 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignInInput } from './dto/signin.input';
 import { SignUpInput } from './dto/signup.input';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-    constructor (
+    constructor(
         private prsimaService: PrismaService,
         private jwt: JwtService
-    ) {}
+    ) { }
 
     async signIn(signInInput: SignInInput) {
-        const user = await this.prsimaService.user.findUnique({
-            where: {
-                email: signInInput.email
+        try {
+            const user = await this.prsimaService.user.findUnique({
+                where: {
+                    email: signInInput.email
+                }
+            })
+    
+            if (!user) {
+                throw new BadRequestException("User not found", { cause: new Error() })
             }
-        })
-
-        if(!user) {
-            throw new Error("User not found")
-        }
-
-        const correctPassword = await bcrypt.compare(signInInput.password, user.password)
-
-        if(!correctPassword) {
-            return new Error("Email or password incorrect")
-        }
-
-        const expiresIn = process.env.JWT_EXPIRES_IN || '24h';
-
-        const payload = {
-            id: user.id,
-            email: user.email
-        }
-
-        return {
-            accessToken: this.jwt.sign(payload, { expiresIn, secret: process.env.JWT_SECRET })
+    
+            const correctPassword = await bcrypt.compare(signInInput.password, user.password)
+    
+            if (!correctPassword) {
+                throw new BadRequestException("User not found", { cause: new Error() })
+            }
+    
+            const expiresIn = process.env.JWT_EXPIRES_IN || '24h';
+    
+            const payload = {
+                id: user.id,
+                email: user.email
+            }
+    
+            return {
+                accessToken: this.jwt.sign(payload, { expiresIn, secret: process.env.JWT_SECRET })
+            }
+        } catch (error) {
+            return error
         }
     }
 
     async signUp(signUpInput: SignUpInput) {
-        let hash = await bcrypt.hash(signUpInput.password, 10)
+        try {
+            let hash = await bcrypt.hash(signUpInput.password, 10)
 
-        const user = await this.prsimaService.user.create({
-            data: {
-                ...signUpInput,
-                password: hash
+            const user = await this.prsimaService.user.create({
+                data: {
+                    ...signUpInput,
+                    password: hash
+                }
+            })
+
+            const expiresIn = process.env.JWT_EXPIRES_IN || '24h';
+
+            const payload = {
+                id: user.id,
+                email: user.email
             }
-        })
 
-        const expiresIn = process.env.JWT_EXPIRES_IN || '24h';
+            return {
+                accessToken: this.jwt.sign(payload, { expiresIn, secret: process.env.JWT_SECRET })
+            }
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new BadRequestException("Email already in use", { cause: new Error() })
+                }
+            }
 
-        const payload = {
-            id: user.id,
-            email: user.email
-        }
-
-        return {
-            accessToken: this.jwt.sign(payload, { expiresIn, secret:  process.env.JWT_SECRET})
+            return new Error("Error, try again later")
         }
     }
 }
